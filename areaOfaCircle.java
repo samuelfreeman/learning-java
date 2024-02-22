7b529fab049593a41511fc81fe163670183d7c00
samuel freeman <samuelmanueljnr@gmail.com>
2024-02-22 21:39:42 +0000

    small changes

--- areaOfaCircle.java
+++ areaOfaCircle.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class areaOfaCircle {
     public static void main(String[] args) {
+
         Scanner myScanner = new Scanner(System.in);
+
         System.out.println("Please enter the Radius of the circle");
+
         double Radius, PI, Area;
+
         PI = 3.147;
+
         Radius = myScanner.nextDouble();
+
         Area = (Radius * Radius) * PI;
+
         System.out.print("The area of the circle is : ");
+
         System.out.println(Area);
     }
 }
